733fba9321412f01295e3a4fde8a25b7a3ce3676
Jorgepaulo12 <112695464+Jorgepaulo12@users.noreply.github.com>
2023-02-13 06:27:51 +0200

    Sim

---
+++ index.js
@@ -0,0 +1,13 @@
+function movitel(){
+ window.location.href="movitel.html"
+}
+
+function Vodacom(){
+ window.location.href="Vodacom.html"
+}
+
+
+function mcel(){
+ 
+window.location.href="mcel.html"
+}
